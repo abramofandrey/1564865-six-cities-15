774a9c3f57f29906e8e6eb8a0cdc6aef708f332c
Abramof Andrey <abramofandrey2@gmail.com>
2024-04-10 23:58:34 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { store } from './store';
-import { fetchOffersAction, checkAuthAction } from './store/api-action';
+import { fetchOffersAction, checkAuthAction, fetchFavoritesAction } from './store/api-action';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,6 +13,12 @@ store.dispatch(fetchOffersAction());
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+store.dispatch(fetchFavoritesAction());
+
+store.dispatch(checkAuthAction());
+store.dispatch(fetchOffersAction());
+store.dispatch(fetchFavoritesAction());
 
 root.render(
   <React.StrictMode>
